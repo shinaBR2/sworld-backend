f0220db3c587cd24de15def2e30c8b1b60b4b188
vuong <shinabr2@gmail.com>
2025-01-10 18:26:27 +0700

    error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import { pino } from "pino";
 import { videosRouter } from "./services/videos";
 import pinoHttp from "pino-http";
+import { errorHandler } from "./error-handler";
 
 const app: Express = express();
 
@@ -27,28 +28,31 @@ const httpLogger = pinoHttp({
       type: req.headers["ce-type"],
       source: req.headers["ce-source"],
     },
+    traceId: req.headers["x-cloud-trace-context"],
   }),
-  // Customize which headers to log
   redact: [
     "req.headers.authorization",
     "req.headers.x-signature",
     "req.headers.x-hub-signature",
     "req.headers.x-webhook-signature",
-    "req.body.*.token", // Redact any token fields in body
-    "req.body.*.password", // Redact any password fields
+    "req.body.*.token",
+    "req.body.*.password",
+    "req.body.*.secret",
+    "req.body.*.key",
   ],
   serializers: {
     req: (req) => ({
       method: req.method,
       url: req.url,
       eventType: req.headers["ce-type"],
+      ip: req.ip,
+      userAgent: req.headers["user-agent"],
     }),
   },
 });
 
-app.use(httpLogger);
-
 // Middlewares
+app.use(httpLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
@@ -58,6 +62,6 @@ app.use(helmet());
 app.use("/videos", videosRouter);
 
 // Error handlers
-// app.use(errorHandler());
+app.use(errorHandler(logger));
 
 export { app, logger };
