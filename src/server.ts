bd4b65b95dd027c209e81beacdc2fda2540d8f0f
vuong <shinabr2@gmail.com>
2025-01-10 17:35:27 +0700

    use logger

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,51 @@ import helmet from "helmet";
 import { pino } from "pino";
 import { videosRouter } from "./services/videos";
 import { envConfig } from "./utils/envConfig";
+import pinoHttp from "pino-http";
 
-const logger = pino({ name: "server start" });
 const app: Express = express();
 
 // Set the application to trust the reverse proxy
 app.set("trust proxy", true);
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
+
+const httpLogger = pinoHttp({
+  logger,
+  customProps: (req, res) => ({
+    cloudEvent: {
+      id: req.headers["ce-id"],
+      type: req.headers["ce-type"],
+      source: req.headers["ce-source"],
+    },
+  }),
+  // Customize which headers to log
+  redact: [
+    "req.headers.authorization",
+    "req.headers.x-signature",
+    "req.headers.x-hub-signature",
+    "req.headers.x-webhook-signature",
+    "req.body.*.token", // Redact any token fields in body
+    "req.body.*.password", // Redact any password fields
+  ],
+  serializers: {
+    req: (req) => ({
+      method: req.method,
+      url: req.url,
+      eventType: req.headers["ce-type"],
+    }),
+  },
+});
+
+app.use(httpLogger);
 
 // Middlewares
 app.use(express.json());
