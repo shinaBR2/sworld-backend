4022bd7fafd364b6e00bd3efbd06385e3b9c5940
vuong <shinabr2@gmail.com>
2025-01-27 14:23:01 +0700

    add unit test

--- src/apps/compute/index.ts
+++ src/apps/compute/index.ts
@@ -1,22 +1,11 @@
-import express, { type Express } from 'express';
-import helmet from 'helmet';
-import { videosRouter } from '../../services/videos';
 import { errorHandler } from '../../utils/error-handler';
-import { httpLogger, logger } from '../../utils/logger';
+import { logger } from '../../utils/logger';
+import { createBaseApp } from 'src/utils/base-app';
+import { videosRouter } from './videos';
 
-const app: Express = express();
+const app = createBaseApp();
 
-// Middlewares
-app.use(httpLogger);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
-
-// Routes
-// app.use("/health-check", healthCheckRouter);
 app.use('/videos', videosRouter);
-
-// Error handlers
 app.use(errorHandler(logger));
 
 export { app };
