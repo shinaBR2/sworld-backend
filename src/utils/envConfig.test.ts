861940c2f99150a1a88c363c0b02807afc843cbc
vuong <shinabr2@gmail.com>
2025-01-27 20:44:23 +0700

    some unit test

---
+++ src/utils/envConfig.test.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { envConfig } from './envConfig';
+
+describe('envConfig', () => {
+  it('has correct default location', () => {
+    expect(envConfig.location).toBe('asia-southeast1');
+  });
+
+  it('contains expected configuration keys', () => {
+    const expectedKeys = [
+      'databaseUrl',
+      'port',
+      'storageBucket',
+      'sentrydsn',
+      'cloudinaryName',
+      'cloudinaryApiKey',
+      'cloudinaryApiSecret',
+      'webhookSignature',
+      'projectId',
+      'computeServiceUrl',
+      'ioServiceUrl',
+      'location',
+    ];
+
+    expectedKeys.forEach(key => {
+      expect(envConfig).toHaveProperty(key);
+    });
+  });
+});
