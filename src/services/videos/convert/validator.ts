672e20d83e32a4d814d7a030942edebb764268c5
vuong <shinabr2@gmail.com>
2025-01-28 17:24:35 +0700

    New validator

--- src/services/videos/convert/validator.ts
+++ src/services/videos/convert/validator.ts
@@ -1,4 +1,10 @@
 import { envConfig } from 'src/utils/envConfig';
+import {
+  FileType,
+  Platform,
+  fileExtensionPatterns,
+  urlPatterns,
+} from 'src/utils/patterns';
 
 const verifySignature = (signature: string | null) => {
   if (!signature) {
@@ -10,4 +16,40 @@ const verifySignature = (signature: string | null) => {
   return signature === webhookSecret;
 };
 
-export { verifySignature };
+interface ValidateMediaURLResult {
+  url: string;
+  platform: Platform | null;
+  fileType: FileType | null;
+}
+
+const validateMediaURL = (url: string): ValidateMediaURLResult => {
+  // Check platform patterns
+  for (const [platform, pattern] of Object.entries(urlPatterns)) {
+    if (pattern.test(url)) {
+      return {
+        url,
+        platform: platform as Platform,
+        fileType: null,
+      };
+    }
+  }
+
+  // Check file extension patterns
+  for (const [type, pattern] of Object.entries(fileExtensionPatterns)) {
+    if (pattern.test(url)) {
+      return {
+        url,
+        platform: null,
+        fileType: type as FileType,
+      };
+    }
+  }
+
+  return {
+    url,
+    platform: null,
+    fileType: null,
+  };
+};
+
+export { verifySignature, validateMediaURL };
