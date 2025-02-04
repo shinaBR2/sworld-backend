a2943740838597b42674c4a86eef1c01effca9be
vuong <shinabr2@gmail.com>
2025-02-04 10:57:01 +0700

    Rollback

--- src/apps/compute/videos/index.ts
+++ src/apps/compute/videos/index.ts
@@ -1,8 +1,10 @@
 import express, { Router } from 'express';
 import { convertHandler } from './routes/convert';
+import { validateRequest } from 'src/utils/validator';
+import { ConvertHandlerRequest, ConvertHandlerSchema } from './routes/convert/schema';
 
 const videosRouter: Router = express.Router();
 
-videosRouter.post('/convert-handler', convertHandler);
+videosRouter.post('/convert-handler', validateRequest<ConvertHandlerRequest>(ConvertHandlerSchema), convertHandler);
 
 export { videosRouter };
