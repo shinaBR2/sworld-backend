f25418fb1b1745612c1615fdd54eeec7d516062f
vuong <shinabr2@gmail.com>
2025-02-05 12:42:35 +0700

    spec

--- src/apps/io/videos/routes/stream-hls/index.ts
+++ src/apps/io/videos/routes/stream-hls/index.ts
@@ -3,8 +3,8 @@ import { completeTask } from 'src/database/queries/tasks';
 import { finalizeVideo } from 'src/database/queries/videos';
 import { videoConfig } from 'src/services/videos/config';
 import { streamM3U8 } from 'src/services/videos/helpers/m3u8';
+import { CustomError } from 'src/utils/custom-error';
 import { logger } from 'src/utils/logger';
-import { AppError } from 'src/utils/schema';
 
 const streamHLSHandler = async (req: Request, res: Response) => {
   const { data, metadata } = req.body;
@@ -29,9 +29,15 @@ const streamHLSHandler = async (req: Request, res: Response) => {
 
     return res.json({ playableVideoUrl });
   } catch (error) {
-    throw AppError('Video conversion failed', {
-      videoId: data.id,
-      error: (error as Error).message,
+    throw CustomError.critical('Stream HLS failed', {
+      originalError: error,
+      errorCode: 'VIDEO_CONVERSION_FAIED',
+      context: {
+        data,
+        metadata,
+        taskId,
+      },
+      source: 'apps/io/videos/routes/stream-hls/index.ts',
     });
   }
 };
