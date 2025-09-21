26f245edc0711be6c078d275dcefac1862bfa67e
shinaBR2 <shinaBR2@users.noreply.github.com>
2025-09-21 13:29:20 +0700

    fix (#109)

--- src/apps/gateway/auth/index.ts
+++ src/apps/gateway/auth/index.ts
@@ -1,28 +1,39 @@
 import { Hono } from 'hono';
 import { deviceRequestCreateSchema } from 'src/schema/auth/device';
-import { requestHandler } from 'src/utils/handlers';
-import { zodValidator } from 'src/utils/validators/zodValidator';
+import { honoRequestHandler } from 'src/utils/requestHandler';
+import { honoValidateRequest } from 'src/utils/validators/request';
 import { createDeviceRequest } from './routes/device';
 
 const authRouter = new Hono();
 
+/**
+ * curl command for local test
+ * curl -X POST 'http://localhost:4000/auth/device' \
+  -H 'Content-Type: application/json' \
+  -H 'x-hasura-action: createDeviceRequest' \
+  -d '{
+    "body": {
+      "action": {
+        "name": "createDeviceRequest"
+      },
+      "input": {
+        "input": {
+          "extensionId": "your-extension-id-here"
+        }
+      }
+    },
+    "headers": {
+      "content-type": "application/json",
+      "x-hasura-action": "createDeviceRequest"
+    },
+    "ip": "127.0.0.1",
+    "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+  }'
+ */
 authRouter.post(
   '/device',
-  zodValidator('json', deviceRequestCreateSchema),
-  requestHandler(createDeviceRequest),
-  // requestHandler(async (context) => {
-  //   const { validatedData } = context;
-  //   const { ip, userAgent } = validatedData;
-  //   const { extensionId } = validatedData.input.input;
-
-  //   const data = await createDeviceRequest({ extensionId, ip, userAgent });
-
-  //   return {
-  //     success: true,
-  //     message: 'ok',
-  //     dataObject: data,
-  //   };
-  // }),
+  honoValidateRequest(deviceRequestCreateSchema),
+  honoRequestHandler(createDeviceRequest),
 );
 
 export { authRouter };
