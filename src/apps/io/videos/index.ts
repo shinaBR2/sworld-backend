0c99bbae3c70349973e2faaf1e0ec33657d4dc77
vuong <shinabr2@gmail.com>
2025-09-21 14:03:51 +0700

    TMP

--- src/apps/io/videos/index.ts
+++ src/apps/io/videos/index.ts
@@ -1,17 +1,5 @@
 import express, { type Router } from 'express';
-import { validateRequest } from 'src/utils/validator';
-import { fixDurationHandler } from './routes/fix-duration';
-import { fixThumbnailHandler } from './routes/fix-thumbnail';
-import { importPlatformHandler } from './routes/import-platform';
-import { streamHLSHandler } from './routes/stream-hls';
-import {
-  type StreamHandlerRequest,
-  streamHandlerSchema,
-} from 'src/schema/videos/stream-hls';
-import {
-  type ImportHandlerRequest,
-  importHandlerSchema,
-} from 'src/schema/videos/import-platform';
+import { Hono } from 'hono';
 import {
   type FixDurationHandlerRequest,
   fixDurationHandlerSchema,
@@ -20,13 +8,28 @@ import {
   type FixThumbnailHandlerRequest,
   fixThumbnailHandlerSchema,
 } from 'src/schema/videos/fix-thumbnail';
+import {
+  type ImportHandlerRequest,
+  importHandlerSchema,
+} from 'src/schema/videos/import-platform';
+import {
+  type StreamHandlerRequest,
+  streamHandlerSchema,
+} from 'src/schema/videos/stream-hls';
+import { honoRequestHandler } from 'src/utils/requestHandler';
+import { validateRequest } from 'src/utils/validator';
+import { honoValidateRequest } from 'src/utils/validators/request';
+import { fixDurationHandler } from './routes/fix-duration';
+import { fixThumbnailHandler } from './routes/fix-thumbnail';
+import { importPlatformHandler } from './routes/import-platform';
+import { streamHLSHandler } from './routes/stream-hls';
 
-const videosRouter: Router = express.Router();
+const videosRouter = new Hono();
 
 videosRouter.post(
   '/stream-hls-handler',
-  validateRequest<StreamHandlerRequest>(streamHandlerSchema),
-  streamHLSHandler,
+  honoValidateRequest(streamHandlerSchema),
+  honoRequestHandler(streamHLSHandler),
 );
 videosRouter.post(
   '/import-platform-handler',
